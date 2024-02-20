eddd92211edffdd8863a7c3520926ead0076776b
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2024-02-20 12:49:35 +0100

    Fix "window is not defined"

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,55 +1,56 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useEffect } from 'react';
 import { OnlineStatusProvider } from "../utils/online";
-import '../styles/global.css'
+import '../styles/global.css';
 
 const App = ({ Component, pageProps }) => {
+	useEffect(() => {
+		if (typeof window !== 'undefined') {
+			window.OneSignal = window.OneSignal || [];
+			window.OneSignal.push(function() {
+				window.OneSignal.init({
+					appId: "0a2f9a03-ae7e-4744-9901-09a0ac1f12f2",
+					safari_web_id: "web.onesignal.auto.0a17e090-f65a-43cf-871a-056959ed633a",
+					notifyButton: {
+						enable: true,
+					},
+				});
+			});
+		}
+	}, []);
 
-  return (
-    <OnlineStatusProvider>
-      <Head>
-        <title>{process.env.NEXT_PUBLIC_WEBSITE_TITLE}</title>
-        <meta charSet='utf-8' />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta
-          name='apple-mobile-web-app-status-bar-style'
-          content='black-translucent'
-        />
-        <meta name='apple-mobile-web-app-title' content={process.env.title} />
-        <meta name='application-name' content={process.env.title} />
-        <meta
-          name='description'
-          content="Accédez à votre emploi du temps en un clin d'oeil"
-        />
-        <meta name='theme-color' content='#1f2022' />
-        <meta name="theme-color" content="#290944" media="(prefers-color-scheme: dark)" />
+	return (
+		<OnlineStatusProvider>
+			<Head>
+				<title>{process.env.NEXT_PUBLIC_WEBSITE_TITLE}</title>
+				<meta charSet='utf-8' />
+				<meta name='mobile-web-app-capable' content='yes' />
+				<meta name='apple-mobile-web-app-capable' content='yes' />
+				<meta
+					name='apple-mobile-web-app-status-bar-style'
+					content='black-translucent'
+				/>
+				<meta name='apple-mobile-web-app-title' content={process.env.title} />
+				<meta name='application-name' content={process.env.title} />
+				<meta
+					name='description'
+					content="Accédez à votre emploi du temps en un clin d'oeil"
+				/>
+				<meta name='theme-color' content='#1f2022' />
+				<meta name="theme-color" content="#290944" media="(prefers-color-scheme: dark)" />
 
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
-        />
-        <link rel='apple-touch-icon' href='/images/maskable_icon_x192.png' />
-        <link rel='icon' type='image/png' href='/favicon.png' />
-        <link rel='manifest' href='/manifest.json' />
-
-				<script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" async=""></script>
-				<script>
-					window.OneSignal = window.OneSignal || [];
-						OneSignal.push(function() {
-							window.OneSignal.init({
-								appId: "0a2f9a03-ae7e-4744-9901-09a0ac1f12f2",
-								safari_web_id: "web.onesignal.auto.0a17e090-f65a-43cf-871a-056959ed633a",
-								notifyButton: {
-									enable: true,
-								},
-							})
-						});
-				</script>
+				<meta
+					name='viewport'
+					content='width=device-width, initial-scale=1, user-scalable=0, viewport-fit=cover'
+				/>
+				<link rel='apple-touch-icon' href='/images/maskable_icon_x192.png' />
+				<link rel='icon' type='image/png' href='/favicon.png' />
+				<link rel='manifest' href='/manifest.json' />
 			</Head>
 
-      <Component {...pageProps} />
-    </OnlineStatusProvider>
-  )
+			<Component {...pageProps} />
+		</OnlineStatusProvider>
+	);
 }
 
-export default App
+export default App;
