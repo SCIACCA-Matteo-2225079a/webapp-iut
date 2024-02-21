79186aa8c7234f2885e680fc572c76157f0c5b3b
Matteo <matteo.sciacca@etu.univ-amu.fr>
2024-02-21 11:43:58 +0100

    Fix alert.tsx

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,16 +1,18 @@
 type Props = {
-    category: string | JSX.Element,
-    content: string,
-    color: string
+	category: string | JSX.Element,
+	content: string,
+	color: string
 }
 
-const Alert = ({ category, content, color }: Props) => <div className="pt-1 m-1 text-center">
-    <div className="px-5">
-        <div className={"inline-flex items-center p-2 text-sm leading-none bg-white rounded-lg shadow text-" + color}>
-            <span className={"inline-flex items-center justify-center h-6 px-3 py-4 font-semibold text-white bg-" + color + "-400 rounded-full text-md sm:text-lg"}>{category}</span>
-            <span className={"inline-flex px-2 text-xs text-black whitespace-pre-line sm:text-base"}>{content}</span>
-        </div>
-    </div>
-</div>
+const Alert = ({ category, content, color }: Props) => (
+	<div className="pt-1 m-1 text-center">
+		<div className="px-5">
+			<div className={`inline-flex items-center p-2 text-sm leading-none bg-white rounded-lg shadow text-${color}`}>
+				<span className={`inline-flex items-center justify-center h-6 px-3 py-4 font-semibold text-white bg-${color}-400 rounded-full text-md sm:text-lg`}>{category}</span>
+				<span className="inline-flex px-2 text-xs text-black whitespace-pre-line sm:text-base">{content}</span>
+			</div>
+		</div>
+	</div>
+);
 
-export default Alert
+export default Alert;
