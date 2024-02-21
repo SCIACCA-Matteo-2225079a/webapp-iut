2e4731a81b72ebc849dbcc339771ff63c42e39f5
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2024-02-21 16:29:01 +0100

    Fix bottom-nav.tsx

--- src/components/bottom-nav.tsx
+++ src/components/bottom-nav.tsx
@@ -3,98 +3,72 @@ import { useRouter } from 'next/router'
 import { Calendar, Home, Rss, Settings } from 'react-feather'
 
 const links = [
-  {
-    title: 'Home',
-    icon: <Home />,
-    href: '/',
-  },
-  {
-    title: 'Emploi du temps',
-    icon: <Calendar />,
-    href: '/schedule',
-  },
-  {
-    title: 'Informations',
-    icon: <Rss />,
-    href: '/news',
-  },
-  {
-    title: 'Paramètres',
-    icon: <Settings />,
-    href: '/settings',
-  },
+	{
+		title: 'Home',
+		icon: <Home />,
+		href: '/',
+	},
+	{
+		title: 'Emploi du temps',
+		icon: <Calendar />,
+		href: '/schedule',
+	},
+	{
+		title: 'Informations',
+		icon: <Rss />,
+		href: '/news',
+	},
+	{
+		title: 'Paramètres',
+		icon: <Settings />,
+		href: '/settings',
+	},
 ]
 
 const BottomNav = () => {
-  const { pathname } = useRouter()
+	const { pathname } = useRouter()
 
-  return (
-    <nav className="bg-white border-t-2 border-gray-100 dark:bg-purple-500 dark:border-opacity-25">
-      <div>
-        {links.map(link => (
-          <Link href={link.href} key={link.title}>
-            <a
-              title={link.title}
-              aria-label={link.title}
-              className={pathname === link.href ? 'active transition-all dark:text-white' : 'transition-all dark:text-white'}
-            >
-              {link.icon}
-            </a>
-          </Link>
-        ))}
-      </div>
-
-      <style jsx>{`
-        nav {
-          padding-bottom: env(safe-area-inset-bottom);
-          width: 100%;
-          height: calc(env(safe-area-inset-top) + 72px);
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          position: fixed;
-          bottom: 0;
-        }
-
-        nav > div {
-          width: 100%;
-          max-width: 480px;
-          height: 100%;
-          display: flex;
-          align-items: center;
-          justify-content: space-around;
-        }
-
-        nav a {
-          width: 48px;
-          height: 48px;
-          border-radius: 50%;
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-
-        nav a :global(> svg) {
-          width: 20px;
-          height: 20px;
-        }
-
-        .active, 
-        nav a:active,
-        nav a:focus,
-        nav a:hover {
-          background: rgba(255, 255, 255, 0.05);
-        }
-
-        body.dark .active,
-        body.dark nav a:active,
-        body.dark nav a:focus,
-        body.dark nav a:hover {
-          background: rgba(0, 0, 0, 0.05);
-        }
-      `}</style>
-    </nav>
-  )
+	return (
+		<nav style={{
+			paddingBottom: 'env(safe-area-inset-bottom)',
+			width: '100%',
+			height: 'calc(env(safe-area-inset-top) + 72px)',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center',
+			position: 'fixed',
+			bottom: 0
+		}}>
+			<div style={{
+				width: '100%',
+				maxWidth: '480px',
+				height: '100%',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'space-around'
+			}}>
+				{links.map(link => (
+					<Link href={link.href} key={link.title}>
+						<a
+							title={link.title}
+							aria-label={link.title}
+							style={{
+								width: '48px',
+								height: '48px',
+								borderRadius: '50%',
+								display: 'flex',
+								alignItems: 'center',
+								justifyContent: 'center'
+							}}
+							className={pathname === link.href ? 'active transition-all dark:text-white' : 'transition-all dark:text-white'}
+						>
+							{link.icon}
+						</a>
+					</Link>
+				))}
+			</div>
+		</nav>
+	)
 }
 
 export default BottomNav
