10b40329d5d51c7143a3cb787265dc59a7912d68
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2024-02-21 16:20:29 +0100

    Fix avatar.tsx

--- src/components/avatar.tsx
+++ src/components/avatar.tsx
@@ -1,47 +1,44 @@
 import { getInitials } from '../helpers'
 
 type Props = {
-  size?: number
-  image?: string
-  children?: React.ReactNode
+	size?: number
+	image?: string
+	children?: React.ReactNode
 }
 
 const Avatar = ({ size = 38, image, children }: Props) => {
-  const string = children ? children.toString() : ''
-  const text = string.split(' ').length > 1 ? getInitials(string) : string[0]
+	const string = children ? children.toString() : ''
+	const text = string.split(' ').length > 1 ? getInitials(string) : string[0]
 
-  return (
-    <div style={{ backgroundImage: image ? `url(${image})` : '' }}>
-      {!image && text}
-
-      <style jsx>{`
-        div {
-          color: white;
-          width: ${size}px;
-          height: ${size}px;
-          line-height: ${size}px;
-          font-size: calc(${size}px * 0.35);
-          font-weight: var(--weight-bold);
-          letter-spacing: 0.035em;
-          background-color: var(--accent);
-          background-size: cover;
-          background-position: center -5px;
-          background-repeat: no-repeat;
-          border-radius: 50%;
-          box-shadow: inset 0 3px 6px rgba(0, 0, 0, 0.1);
-          display: flex;
-          align-items: center;
-          justify-content: center;
-        }
-      `}</style>
-    </div>
-  )
+	return (
+		<div style={{
+			backgroundImage: image ? `url(${image})` : '',
+			color: 'white',
+			width: `${size}px`,
+			height: `${size}px`,
+			lineHeight: `${size}px`,
+			fontSize: `calc(${size}px * 0.35)`,
+			fontWeight: 'var(--weight-bold)',
+			letterSpacing: '0.035em',
+			backgroundColor: 'var(--accent)',
+			backgroundSize: 'cover',
+			backgroundPosition: 'center -5px',
+			backgroundRepeat: 'no-repeat',
+			borderRadius: '50%',
+			boxShadow: 'inset 0 3px 6px rgba(0, 0, 0, 0.1)',
+			display: 'flex',
+			alignItems: 'center',
+			justifyContent: 'center'
+		}}>
+			{!image && text}
+		</div>
+	)
 }
 
 Avatar.defaultProps = {
-  size: 38,
-  image: null,
-  children: null
+	size: 38,
+	image: null,
+	children: null
 }
 
 export default Avatar
