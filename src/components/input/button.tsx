75b977a782c3f85d52f8d9f13bcc8ca607ed4cee
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2024-02-22 15:33:31 +0100

    Fix button.tsx 9

--- src/components/input/button.tsx
+++ src/components/input/button.tsx
@@ -28,7 +28,7 @@ const Brand = ({ children, onClick, href, icon, disabled = false, type }: Props)
 			className="flex px-4 py-2 my-4 font-semibold text-white bg-blue-500 rounded-lg shadow-md justify-evenly hover:bg-blue-700 dark:bg-gold-500 dark:hover:bg-gold-700 focus:outline-none disabled:opacity-50"
 		>
 			<div className="mr-2">
-				{icon || <Package />} {/* Utilisation d'un OR logique pour la valeur par défaut */}
+				{icon || <Package />} {}
 			</div>
 			{children}
 		</button>
