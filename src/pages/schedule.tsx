86ca9085e3f783a2cfee65d496b942c9273a4d38
BENDJEDDOU-Rayan-2225011a <rayan.bendjeddou@etu.univ-amu.fr>
2024-02-22 10:48:34 +0100

    fix pages/schedule.tsx

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,5 +1,7 @@
 import useSWR from 'swr';
 
+import { useEffect } from 'react';
+import { AlertTriangle } from 'react-feather';
 import Page from '../components/page'
 import Section from '../components/section'
 import Schedule from '../components/schedule'
@@ -7,8 +9,6 @@ import Alert from '../components/alert'
 
 import { useOnlineStatus } from '../utils/online';
 import useLocalStorage from '../utils/localStorage';
-import { useEffect, useState } from 'react';
-import { AlertTriangle } from 'react-feather';
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
@@ -19,8 +19,6 @@ const SchedulePage = () => {
   const [code] = useLocalStorage("schedule.adeCode", 8379);
   const [scheduleData, setScheduleData] = useLocalStorage("schedule.data", []);
 
-  const [showPassedCourses, setPassedCoursesVisibility] = useState(false);
-
   const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_ECRANS_CONNECTES_URL || 'https://iut-ptut.alwaysdata.net/wp-json/amu-ecran-connectee'}/v1/schedule/${code}`, fetcher)
 
   useEffect(() => {
@@ -29,15 +27,15 @@ const SchedulePage = () => {
     console.log(new Date());
     setScheduleData(data.data);
     setLastUpdate(new Date());
-  }, [data])
+  }, [data, error, setLastUpdate, setScheduleData])
 
   console.dir(scheduleData);
 
   return (
-    <Page title='Emploi du temps' showButtons={true}>
-      {!isOnline && lastUpdate && <Alert category={<AlertTriangle />} color="yellow" content={"Vous êtes hors-ligne. L'emploi du temps à été mis-à-jour pour la dernière fois le: " + new Date(lastUpdate).toLocaleString('fr-FR', { hour: 'numeric', minute: 'numeric', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })} />}
+    <Page title='Emploi du temps' showButtons>
+      {!isOnline && lastUpdate && <Alert category={<AlertTriangle />} color="yellow" content={`Vous êtes hors-ligne. L'emploi du temps à été mis-à-jour pour la dernière fois le: ${  new Date(lastUpdate).toLocaleString('fr-FR', { hour: 'numeric', minute: 'numeric', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`} />}
       {!isOnline && !lastUpdate && <Alert category={<AlertTriangle />} color="yellow" content="Vous êtes hors-ligne. L'emploi du temps est inaccessible. Reconnectez-vous pour l'avoir à jour." />}
-      {error && lastUpdate && <Alert category={<AlertTriangle />} color="yellow" content={"Impossible de se connecter au serveur des écrans connectés. L'emploi du temps à été mis-à-jour pour la dernière fois le: " + new Date(lastUpdate).toLocaleString('fr-FR', { hour: 'numeric', minute: 'numeric', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })} />}
+      {error && lastUpdate && <Alert category={<AlertTriangle />} color="yellow" content={`Impossible de se connecter au serveur des écrans connectés. L'emploi du temps à été mis-à-jour pour la dernière fois le: ${  new Date(lastUpdate).toLocaleString('fr-FR', { hour: 'numeric', minute: 'numeric', weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`} />}
       <Section>
         {scheduleData && <Schedule data={scheduleData} />}
       </Section>
