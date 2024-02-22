e2d837b2fe126938ea48e8a53b8126df6b0b7c3f
SCIACCA-Matteo-2225079a <matteo.sciacca@etu.univ-amu.fr>
2024-02-22 12:35:26 +0100

    Fix button.tsx 3

--- src/components/input/button.tsx
+++ src/components/input/button.tsx
@@ -12,12 +12,25 @@ type Props = {
 }
 
 const Brand = ({ children, onClick, href, icon, disabled = false, type = "button" }: Props) => (
-	<button type={type} disabled={disabled} onClick={onClick ? onClick : () => href ? router.push('/settings') : undefined} className="flex px-4 py-2 my-4 font-semibold text-white bg-blue-500 rounded-lg shadow-md justify-evenly hover:bg-blue-700 dark:bg-gold-500 dark:hover:bg-gold-700 focus:outline-none disabled:opacity-50">
+	<button
+		type={type === "button" || type === "submit" || type === "reset" ? type : "button"}
+		disabled={disabled}
+		onClick={onClick ? onClick : () => href ? router.push('/settings') : undefined}
+		className="flex px-4 py-2 my-4 font-semibold text-white bg-blue-500 rounded-lg shadow-md justify-evenly hover:bg-blue-700 dark:bg-gold-500 dark:hover:bg-gold-700 focus:outline-none disabled:opacity-50"
+	>
 		<div className="mr-2">
 			{icon ? icon : <Package />}
 		</div>
 		{children}
 	</button>
 )
+
+Brand.defaultProps = {
+	disabled: false,
+	onClick: undefined,
+	type: "button",
+	href: undefined,
+	icon: undefined
+};
 
 export default Brand;
