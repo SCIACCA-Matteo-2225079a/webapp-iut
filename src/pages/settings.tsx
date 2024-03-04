6b62172e4490e4752337386c4754e6ccd9074530
BENDJEDDOU-Rayan-2225011a <rayan.bendjeddou@etu.univ-amu.fr>
2024-03-04 16:43:39 +0100

    tentative fix

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -35,48 +35,48 @@ const Settings = () => {
 			}
     }
 
-    return (
-        <Page title='Paramètres' subtitle={<p className="text-sm dark:text-gray-200">Ces paramètres seront enregistrés sur votre appareil.</p>}>
-            <Section>
-                <Section>
-                    <h3 className="text-lg font-semibold">Notifications</h3>
-                    <p className="text-xs dark:text-gray-200">Recevez des notifications de la part de l&apos;IUT en cas d&apos;informations urgentes.</p>
-                    <Button icon={notifications ? <BellOff /> : <Bell />} onClick={() => setNotifications(!notifications)}>
-                        <span>{notifications ? 'Désactiver' : 'Activer'}</span>
-                    </Button>
-                    {!isMobileSafari && <p className="text-xs dark:text-gray-200">iOS ne permet pas à l&apos;heure actuelle de recevoir les notifications d&apos;un site internet. Veuillez utiliser un autre navigateur; comme <b>Chrome</b> ou <b>Firefox</b>.</p>}
-                </Section>
+	return (
+		<Page title='Paramètres' subtitle={<p className="text-sm dark:text-gray-200">Ces paramètres seront enregistrés sur votre appareil.</p>}>
+			<Section>
+				<Section>
+					<h3 className="text-lg font-semibold">Notifications</h3>
+					<p className="text-xs dark:text-gray-200">Recevez des notifications de la part de l&apos;IUT en cas d&apos;informations urgentes.</p>
+					<Button icon={notifications ? <BellOff /> : <Bell />} onClick={() => setNotifications(!notifications)}>
+						<span>{notifications ? 'Désactiver' : 'Activer'}</span>
+					</Button>
+					{!isMobileSafari && <p className="text-xs dark:text-gray-200">iOS ne permet pas à l&apos;heure actuelle de recevoir les notifications d&apos;un site internet. Veuillez utiliser un autre navigateur; comme <b>Chrome</b> ou <b>Firefox</b>.</p>}
+				</Section>
 
-                <Section disabled={error !== null} loading={!Array.isArray(data)}>
-                    <h3 className="text-lg font-semibold">Emploi du temps sélectionné</h3>
-                    <p className="mb-4 text-sm dark:text-gray-200">Vous pouvez le changer à tout moment. Il est accessible hors-ligne.</p>
+				<Section disabled={error !== null} loading={!Array.isArray(data)}>
+					<h3 className="text-lg font-semibold">Emploi du temps sélectionné</h3>
+					<p className="mb-4 text-sm dark:text-gray-200">Vous pouvez le changer à tout moment. Il est accessible hors-ligne.</p>
 
-                    <Pills currentValue={groupFilter} onValueChange={setGroupFilter} data={[
-                        { value: true, label: 'Tout' },
-                        { value: 'year', label: 'Année' },
-                        { value: 'group', label: 'Groupe' },
-                        { value: 'halfGroup', label: 'Demi-groupe' },
-                        { value: 'teacher', label: 'Enseignant' }
-                    ]} />
+					<Pills currentValue={groupFilter} onValueChange={setGroupFilter} data={[
+						{ value: true, label: 'Tout' },
+						{ value: 'year', label: 'Année' },
+						{ value: 'group', label: 'Groupe' },
+						{ value: 'halfGroup', label: 'Demi-groupe' },
+						{ value: 'teacher', label: 'Enseignant' }
+					]} />
 
-                    <div className="relative inline-block w-full mt-4 text-gray-700">
-                        {data && (<>
-                            <select onChange={e => onCodeChange(e.target.value)} className="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none dark:text-gray-100 dark:bg-purple-700 dark:border-transparent focus:outline-none focus:ring focus:border-blue-300" placeholder="Emploi du temps à sélectionner" defaultValue={code}>
-															{Array.isArray(data) && data.filter(({ type }) => groupFilter === true || type === groupFilter)
-																.map(({ newCode, title }) => <option key={newCode} value={newCode}>{title}</option>)}
-														</select>
-                            <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
-                                <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
-                                    <path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd" />
-                                </svg>
-                            </div>
-                        </>)}
-                    </div>
-                </Section>
-            </Section>
+					<div className="relative inline-block w-full mt-4 text-gray-700">
+						{data && (<>
+							<select onChange={e => onCodeChange(parseInt(e.target.value))} className="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none dark:text-gray-100 dark:bg-purple-700 dark:border-transparent focus:outline-none focus:ring focus:border-blue-300" placeholder="Emploi du temps à sélectionner" defaultValue={code}>
+								{data.filter(({ type }) => groupFilter === true || type === groupFilter)
+									.map(({ code, title }, i: number) => <option key={i} value={code}>{title}</option>)}
+							</select>
+							<div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+								<svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+									<path d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" fillRule="evenodd" />
+								</svg>
+							</div>
+						</>)}
+					</div>
+				</Section>
+			</Section>
 
-        </Page>
-        )
-    }
+		</Page>
+	)
+}
 
 export default Settings
